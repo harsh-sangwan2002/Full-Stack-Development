06c1d3c316403f018b84219ca93500f3661ded38
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-24 17:23:53 +0530

    strings started

---
+++ JavaScript/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,11 @@
+let x;
+
+const name = 'John';
+const age = 30;
+
+x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old.';
+console.log(x);
+
+// Template literal
+x = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(x);
