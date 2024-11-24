cbe7b710ac96f8dbab6a9ab265928f73a8e14619
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-24 16:36:06 +0530

    type-coercion done

---
+++ JavaScript/01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,16 @@
+let name = "John";
+let age = 23;
+
+let newName = name;
+newName = "Jonathon";
+
+console.log(name, newName);
+
+let person = {
+    name: "Brad",
+    age: 40,
+}
+let newPerson = person;
+newPerson.name = "Steve";
+
+console.log(person, newPerson);
