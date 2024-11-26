35a15aec797e9b223259f687faf583dc3a9954db
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-26 19:01:39 +0530

    number done

---
+++ JavaScript/01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,14 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+let sum = x + y;
+let diff = x - y;
+let product = x * y;
+let quotient = x / y;
+let rem = x % y;
+
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(quotient);
+console.log(rem);
