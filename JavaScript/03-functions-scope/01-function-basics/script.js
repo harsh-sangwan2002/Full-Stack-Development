d9c7dfb817d6c9bbe9b6aff4324df8d42f015c2b
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-27 11:26:56 +0530

    function basics & arrays done

---
+++ JavaScript/03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    console.log("Hello");
+}
+sayHello();
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+add(10,2);
+
+function subtract(num1,num2){
+    return num1-num2;
+}
+const res = subtract(5,2);
+console.log(res);
