914117b5d62c55049449bd6e9df41aabae37178d
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-28 12:39:07 +0530

    logic & conditionals done

---
+++ JavaScript/04-logic-control-flow/01-if-statements/script.js
@@ -0,0 +1,24 @@
+if (true) {
+    console.log('This is true');
+}
+
+if (false) {
+    console.log('This is not true');
+}
+
+const x = 10, y = 5;
+
+if (x > y) {
+    console.log(`${x} is greater than ${y}`);
+}
+else if (x === y) {
+    console.log(`${x} is equal to ${y}`);
+}
+else {
+    console.log(`${y} is greater than ${x}`);
+}
+
+if (x !== y) {
+    const z = 20;
+    console.log(`${z} is 20`);
+}
