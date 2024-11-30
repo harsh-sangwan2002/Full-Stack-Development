4592eee636465cc66e64a8e48d9d7b3da30f6d50
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-11-30 15:05:19 +0530

    traversong the dom done

---
+++ JavaScript/06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -0,0 +1,28 @@
+let output;
+
+// Get child elements
+const parent = document.querySelector('.parent');
+output = parent.children;
+output = parent.children[1].innerText;
+output = parent.children[1].className;
+output = parent.children[1].nodeName;
+
+parent.children[1].innerText = 'Child Two';
+
+parent.firstElementChild.innerText = 'Child One';
+parent.lastElementChild.innerText = 'Child Three';
+
+// Get parent element from child
+const child = document.querySelector('.child');
+output = child.parentElement;
+child.parentElement.style.border = `1px solid black`;
+child.parentElement.style.padding = `15px 10px`;
+
+// Sibling elements
+const secondItem = document.querySelector('.child:nth-child(2)');
+output = secondItem;
+output = secondItem.nextElementSibling;
+secondItem.previousElementSibling.style.color = 'red';
+secondItem.nextElementSibling.style.color = 'green';
+
+console.log(output);
