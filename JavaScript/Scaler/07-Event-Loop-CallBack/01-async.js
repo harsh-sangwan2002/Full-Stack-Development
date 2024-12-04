c03dd475b4a98c0a84a3dc038aea1ec49556d6fe
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-12-04 12:58:59 +0530

    event loop & callback started

---
+++ JavaScript/Scaler/07-Event-Loop-CallBack/01-async.js
@@ -0,0 +1,61 @@
+/******************** Synchronous Code : That code executes line by line *******************/
+console.log("Before");
+
+function fn(){
+    console.log("I am fn function.");
+}
+fn();
+console.log("After");
+
+/******************** Asynchronous Code : Some code is executed at current point of time and some at later point of time. *******************/
+console.log("Before");
+
+setTimeout(()=>{
+    console.log("I am fn function.");
+},2000);
+
+console.log("After");
+
+/************************ Enviornment : Browser
+    Web API :
+     * console -> is not a part of js
+     * window -> is not a part of js
+     * document -> is not a part of js
+     * fetch -> is not a part of js
+     * eventListners -> is not a part of js
+     *
+ * Enviornment : Nodejs
+     * fs -> is not a part of js
+     * http -> nodejs
+     * child_process -> nodejs
+     * 
+ * Thumb rules:
+ *     -> Environment provide you the features.
+ *     -> Js provide you the logic.
+ *     -> Programmers:
+ *                   [C++ -> Java] : Pointers were taken care of.
+ *                   [Java -> JS]  : There are no threds to take care of.
+ *    Inference : You cannot create an asynchronous fns as a programmer -> Enviornment
+ * */
+
+console.log("Before");
+const cb2 = ()=>{
+    console.log("set timeout:");
+    let timeInFuture = Date.now() + 5000;
+
+    let before = Date.now();
+    console.log("Before while begins: ",before);
+    while(Date.now()<timeInFuture){
+
+    }
+
+    let after = Date.now();
+    console.log("After while loop: ",after);
+    console.log("Time spent: ",after-before);
+}
+
+const cb1 = ()=> console.log("Hello");
+setTimeout(cb1,1000);
+setTimeout(cb2,2000);
+
+console.log("After");
