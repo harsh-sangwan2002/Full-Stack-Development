1a7836e91db33a63764d06d9513d5d10eb9e418f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-12-05 20:28:37 +0530

    star-component started

---
+++ Frontend-Machine-Coding/03-Machine-Coding-Case-Studies/01-Star-Component/index.js
@@ -0,0 +1,31 @@
+const starContainer = document.querySelector('#star-container');
+const stars = document.querySelectorAll('.star');
+const count = document.querySelector('#count');
+
+let clickedStarIdx = 0; // Index of the last clicked star
+let currentHoverIdx = -1; // Index of the currently hovered star
+
+// On mouse click
+starContainer.addEventListener('click', (e) => {
+    if (e.target.hasAttribute("idx")) {
+        clickedStarIdx = parseInt(e.target.getAttribute("idx"));
+        fillStarUptoIdx(clickedStarIdx);
+        updateCount();
+    }
+});
+
+function updateCount() {
+    count.textContent = clickedStarIdx;
+}
+
+function resetAllStars() {
+    stars.forEach(star => star.classList.remove("yellow"));
+}
+
+function fillStarUptoIdx(idx) {
+    resetAllStars();
+
+    for (let i = 0; i < idx; i++) {
+        stars[i].classList.add('yellow');
+    }
+}
