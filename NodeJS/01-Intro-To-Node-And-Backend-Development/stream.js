706e642d3ade7a17c27ddbf651ada8c6a4938787
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-12-11 20:08:07 +0530

    intro to node done

---
+++ NodeJS/01-Intro-To-Node-And-Backend-Development/stream.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const srcFilePath = './dir/file1.txt';
+const destFilePath = './dir/file2.txt';
+
+const readStream = fs.createReadStream(srcFilePath);
+const writeStream = fs.createWriteStream(destFilePath);
+
+readStream.pipe(writeStream);
+readStream.on('data', data => {
+    console.log(data.toString());
+})
+readStream.on('end', () => {
+    console.log("Stream end");
+})
+readStream.on('error', err => {
+    console.log(err);
+})
