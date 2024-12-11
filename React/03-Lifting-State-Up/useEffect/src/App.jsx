30d7eadb59a2f7cc23d30a832bc25d968c713464
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-12-11 13:10:13 +0530

    useEffect done

---
+++ React/03-Lifting-State-Up/useEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HandleAllTodo from './components/HandleAllTodo'
+import GetData from './components/useEffectExamples/getData'
+import UseEffectExample from './components/useEffectExampless/UseEffectExample'
+
+function App() {
+
+  return (
+    // <GetData />
+    <UseEffectExample />
+  )
+}
+
+export default App
