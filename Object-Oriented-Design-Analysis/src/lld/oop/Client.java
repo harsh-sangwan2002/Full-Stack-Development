b15ef82451e46c96a7a51856d19abcf04075ef93
Harsh Sangwan <hsangwan2002@gmail.com>
2024-12-19 08:58:10 +0530

    updated llp oops

--- Object-Oriented-Design-Analysis/src/lld/oop/Client.java
+++ Object-Oriented-Design-Analysis/src/lld/oop/Client.java
@@ -1,4 +1,19 @@
 package lld.oop;
 
-public class client {
+public class Client {
+
+    public static void main(String[] args) {
+
+        Student st1 = new Student();
+        st1.name = "Vinay";
+        st1.batch = "May2024";
+
+
+        Student st2 = new Student();
+        st2.name = "Harsh";
+        st2.batch = "May2024";
+        st2.psp = 92.02;
+        st2.applyForJob(123);
+        st2.applyForJob(1234);
+    }
 }
