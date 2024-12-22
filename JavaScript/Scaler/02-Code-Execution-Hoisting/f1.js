8cc3e0539ea65dc1f58002f2e36fd211063c59c9
Harsh Sangwan <hsangwan2002@gmail.com>
2024-12-22 09:12:22 +0530

    completed types of function and usecases

---
+++ JavaScript/Scaler/02-Code-Execution-Hoisting/f1.js
@@ -0,0 +1,19 @@
+// Named function
+function sum(a,b,c){
+    let total = a+b+c;
+    return total;
+}
+console.log(sum(1,2,3,4));
+
+// Anonymous function
+const sum1 = function(a,b,c){
+    let total = a+b+c;
+    return total;
+}
+console.log(sum1(1,2,3,4));
+
+// Arrow function
+const sum2 = (a,b,c)=>{
+    let total = a+b+c;
+    return total;
+}
