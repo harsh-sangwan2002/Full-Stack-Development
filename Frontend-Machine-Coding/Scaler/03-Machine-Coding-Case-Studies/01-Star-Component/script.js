19b2951b9e5c6638dcba626862d7dd230e8aad24
Harsh Sangwan <hsangwan2002@gmail.com>
2024-12-24 11:53:04 +0530

    updated star component

--- Frontend-Machine-Coding/Scaler/03-Machine-Coding-Case-Studies/01-Star-Component/script.js
+++ Frontend-Machine-Coding/Scaler/03-Machine-Coding-Case-Studies/01-Star-Component/script.js
@@ -1,45 +1,47 @@
 const starContainer = document.querySelector('#star-container');
-const stars = document.querySelectorAll('.star');
-const count = document.querySelector('#count');
+const allStars = document.querySelectorAll('.star');
+const ratingCount = document.querySelector('#count');
 
-let clickedStarIdx = 0; // Index of the last clicked star
-let currentHoverIdx = -1; // Index of the currently hovered star
+let clickedStarIdx = 0;
+let currentHoverIdx = -1;
 
-// On mouse click
 starContainer.addEventListener('click', (e) => {
-    if (e.target.hasAttribute("idx")) {
-        clickedStarIdx = parseInt(e.target.getAttribute("idx"));
+
+    const target = e.target;
+    if (target.classList.contains('star')) {
+        clickedStarIdx = parseInt(target.getAttribute('data-idx'));
         fillStarUptoIdx(clickedStarIdx);
         updateCount();
     }
-});
+})
 
-// On mouse hover
-starContainer.addEventListener("mouseover",(e)=>{
-    if(e.target.hasAttribute("idx")){
-        currentHoverIdx = parseInt(e.target.getAttribute("idx"));
+starContainer.addEventListener('mouseover', e => {
+
+    const target = e.target;
+    if (target.classList.contains('star')) {
+        currentHoverIdx = target.getAttribute('data-idx');
         fillStarUptoIdx(currentHoverIdx);
     }
 })
 
-// On mouse leave
-starContainer.addEventListener("mouseleave",(e)=>{
+starContainer.addEventListener('mouseleave', e => {
+
     resetAllStars();
     fillStarUptoIdx(clickedStarIdx);
 })
 
 function updateCount() {
-    count.textContent = clickedStarIdx;
+    ratingCount.innerText = clickedStarIdx;
 }
 
 function resetAllStars() {
-    stars.forEach(star => star.classList.remove("yellow"));
+    allStars.forEach(star => {
+        star.classList.remove('yellow');
+    })
 }
-
 function fillStarUptoIdx(idx) {
     resetAllStars();
 
-    for (let i = 0; i < idx; i++) {
-        stars[i].classList.add('yellow');
-    }
+    for (let i = 0; i < idx; i++)
+        allStars[i].classList.add('yellow');
 }
