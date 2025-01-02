384f56c7b02b76feadbc107b9537640612162bdf
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-02 10:07:33 +0530

    Stopwatch component completed

--- ReactJS/03-React-Machine-Coding/todo-app/src/App.jsx
+++ ReactJS/03-React-Machine-Coding/todo-app/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Todo from "./components/Todo/Todo";
 import GetData from "./components/useEffect/GetData";
+import StopWatch from './components/StopWatch/StopWatch';
 
 function App() {
 
   return (
     <>
       {/* <Todo /> */}
-      <GetData />
+      {/* <GetData /> */}
+      <StopWatch />
     </>
   )
 }
