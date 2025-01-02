b0af77a617d5650e110caf0d9432dd8fad0641e4
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-02 16:21:22 +0530

    infinte scroll and intersection observer done

--- ReactJS/04-React-Router-Hooks/infinite-scroll/src/components/InfiniteScroll.jsx
+++ ReactJS/04-React-Router-Hooks/infinite-scroll/src/components/InfiniteScroll.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
-import styleObject from "./style.module.css";
-import useFetchParams from "./useFetchParams";
+import React, { useState } from 'react'
+// using normal styling sheet is not a good practice
+// import ./style.css
+
+// Resolves the confilicts of class names
+import styleObject from './style.module.css'
+import useFetchParams from './useFetchParams'
 
 function InfiniteScroll() {
+
+    // Data -> Fetch from an api
+    // Next -> Next api call will be made
     const [page, setPage] = useState(1);
     const [posts, error, loader] = useFetchParams(page);
 
     return (
         <div>
-            {posts.map((post, idx) => (
-                <div className={styleObject.book_title} key={idx}>
-                    {post.title}
-                </div>
-            ))}
-            {loader && <div className={styleObject.loader}>Loading...</div>}
-            {error && <div className={styleObject.error}>{error}</div>}
-            <button onClick={() => setPage((prevPage) => prevPage + 1)}>Next</button>
+            {
+                posts.map((post, idx) => (
+                    <div className={styleObject.book_title} key={idx}>{post.title}</div>
+                ))
+            }
+            {loader && <button className={styleObject.loader}></button>}
+            {error && <div>{error}</div>}
             <div>{page}</div>
+            <button onClick={() => setPage(page + 1)}>Next</button>
+
         </div>
-    );
+    )
 }
 
-export default InfiniteScroll;
+export default InfiniteScroll
