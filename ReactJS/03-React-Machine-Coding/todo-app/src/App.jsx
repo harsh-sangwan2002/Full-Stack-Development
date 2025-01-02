f0dc5b61b22421263e94cb9e9b20cdb7dea53f9d
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-02 09:41:42 +0530

    fetch Data done

--- ReactJS/03-React-Machine-Coding/todo-app/src/App.jsx
+++ ReactJS/03-React-Machine-Coding/todo-app/src/App.jsx
@@ -1,10 +1,14 @@
-import Todo from "./components/Todo/Todo";
 import './App.css';
+import Todo from "./components/Todo/Todo";
+import GetData from "./components/useEffect/GetData";
 
 function App() {
 
   return (
-    <Todo />
+    <>
+      {/* <Todo /> */}
+      <GetData />
+    </>
   )
 }
 
--- ReactJS/03-React-Machine-Coding/todo-app/src/main.jsx
+++ ReactJS/03-React-Machine-Coding/todo-app/src/main.jsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // In dev-strict mode useEffect will be called twice
   <App />,
 )
