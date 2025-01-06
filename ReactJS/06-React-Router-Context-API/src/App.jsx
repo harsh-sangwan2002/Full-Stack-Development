6530c90600b139f503fe739ee5ed8b3644086b2d
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-06 12:43:35 +0530

    completed react-router-dom

--- ReactJS/06-React-Router-Context-API/src/App.jsx
+++ ReactJS/06-React-Router-Context-API/src/App.jsx
@@ -1,9 +1,22 @@
-import Routing from "./components/Routing"
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from './pages/Home';
+import Profile from "./pages/Profile";
+import ProductDetails from "./pages/ProductDetails";
+import PageNotFound from "./pages/PageNotFound";
+import NavBar from "./components/NavBar";
+import './index.css';
 
 function App() {
   return (
     <>
-      <Routing></Routing>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}> </Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/product/:id" element={<ProductDetails></ProductDetails>}> </Route>
+        <Route path="/home" element={<Navigate to="/"></Navigate>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}> </Route>
+      </Routes>
     </>
   )
 }
