e24901f58e81a182e830d57a54c0ff19dec1b3c4
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-07 19:19:37 +0530

    Intro To Threads & processes done

---
+++ Backend-Development/LLD_1/src/concurrencyIntroToProcesses/HelloWorldPrinter.java
@@ -0,0 +1,9 @@
+package concurrencyIntroToProcesses;
+
+public class HelloWorldPrinter implements Runnable{
+
+    @Override
+    public void run(){
+        System.out.println("Hello World: Thread name = "+Thread.currentThread().getName());
+    }
+}
