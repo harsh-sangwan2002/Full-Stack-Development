7883f955366e2672a4f83fcc1b866397598d1893
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-09 10:28:58 +0530

    updated banner css

--- ReactJS/YouTube/movies-app/src/components/Banner.jsx
+++ ReactJS/YouTube/movies-app/src/components/Banner.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
-import { movies } from '../utils/getMovies'
+import React, { Component } from 'react';
+import { movies } from '../utils/getMovies';
 
 export default class Banner extends Component {
-
     render() {
         let movie = movies.results[0];
 
         return (
             <div>
-                {
-                    movie.length == 0 ? <div class="spinner-border text-primary" role="status">
+                {movie.length === 0 ? (
+                    <div className="spinner-border text-primary" role="status">
                         <span className="visually-hidden">Loading...</span>
-                    </div> :
-
-                        <div className="card banner-card" style={{ width: '18rem' }}>
-                            <img src={` https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="banner-img card-img-top" alt="..." />
-                            <h5 className="banner-title">{movie.title}</h5>
-                            <p className="card-text banner-text">{movie.overview}</p>
-                        </div>
-
-                }
+                    </div>
+                ) : (
+                    <div className="banner-card">
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                            className="banner-img card-img-top"
+                            alt={movie.title}
+                        />
+                        <h5 className="banner-title">{movie.title}</h5>
+                        <p className="card-text banner-text">{movie.overview}</p>
+                    </div>
+                )}
             </div>
-        )
+        );
     }
 }
