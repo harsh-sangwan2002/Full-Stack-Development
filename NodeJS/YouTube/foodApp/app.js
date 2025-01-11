f9fbaf35b705323ea0080f67b1a7cd7653de87e4
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-11 13:48:39 +0530

    initialised food-app

--- NodeJS/YouTube/foodApp/app.js
+++ NodeJS/YouTube/foodApp/app.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.json());
 
-let users = {};
+// mini app
+const userRouter = express.Router();
+const authRouter = express.Router();
 
-app.get('/users', (req, res) => {
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
+
+const getSignUp = (req, res) => {
+    res.sendFile('/public/index.html', { root: __dirname });
+}
+
+const postSignUp = (req, res) => {
+    const body = req.body;
+    res.json({
+        message: "Data recieved successfully",
+        body: body
+    })
+    console.log(body);
+}
+
+const getUser = (req, res) => {
     res.send(users);
-})
+}
 
-app.post('/users', (req, res) => {
+const postUser = (req, res) => {
     console.log(req.body);
     users = req.body.name;
     res.json({
         message: "Data received successfully.",
         user: req.body
     })
-})
+}
 
-app.patch('/users', (req, res) => {
+const updateUser = (req, res) => {
     console.log(req.body);
 
     const datatoBeUpdated = req.body;
@@ -30,16 +49,38 @@ app.patch('/users', (req, res) => {
         message: "Data updated successfully.",
         user: req.body
     })
-})
+}
 
-app.delete('/users', (req, res) => {
+const deleteUser = (req, res) => {
     users = {};
     res.json({
         message: "Data deleted successfully.",
         user: users
     })
+}
+
+const getUserById = (req, res) => {
+    res.send(req.params.id);
+}
+
+app.get('/user', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
 })
 
-app.listen(3000, () => {
-    console.log("Backend server is running.");
+userRouter.route('/')
+    .get(getUser)
+    .post(postUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+userRouter.route('/:id')
+    .get();
+
+authRouter.route('/signup')
+    .get(getSignUp)
+    .post(postSignUp);
+
+app.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000");
 })
