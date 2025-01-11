c8833a00a8bf1753fd858186db5f80f4c0291255
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-11 13:11:37 +0530

    mounting in express done

--- NodeJS/YouTube/03-Users/methods.js
+++ NodeJS/YouTube/03-Users/methods.js
@@ -3,9 +3,56 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/user/:id', (req, res) => {
+// mini app
+const userRouter = express.Router();
+app.use('/user', userRouter);
+userRouter.route('/')
+    .get(getUser)
+    .post(postUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+userRouter.route('/:id')
+    .get();
+
+const getUser = (req, res) => {
+    res.send(users);
+}
+
+const postUser = (req, res) => {
+    console.log(req.body);
+    users = req.body.name;
+    res.json({
+        message: "Data received successfully.",
+        user: req.body
+    })
+}
+
+const updateUser = (req, res) => {
+    console.log(req.body);
+
+    const datatoBeUpdated = req.body;
+    for (let key in datatoBeUpdated) {
+        users[key] = datatoBeUpdated[key];
+    }
+
+    res.json({
+        message: "Data updated successfully.",
+        user: req.body
+    })
+}
+
+const deleteUser = (req, res) => {
+    users = {};
+    res.json({
+        message: "Data deleted successfully.",
+        user: users
+    })
+}
+
+const getUserById = (req, res) => {
     res.send(req.params.id);
-})
+}
 
 app.get('/user', (req, res) => {
     console.log(req.query);
