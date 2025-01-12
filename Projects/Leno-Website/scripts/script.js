3ec6790bf91449b68d278f8038580ebc0e350fa9
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-12 08:52:40 +0530

    removed project directory

--- Projects/Leno-Website/scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-
-    const menuIcon = document.querySelector('.navbar__mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
-
-    menuIcon.addEventListener('click',()=>{
-        mobileMenu.classList.toggle('active');
-    })
-})
