4bad6c32eb522e5de424bef2c4bb7c7a6bb44903
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-13 14:33:16 +0530

    added ReactJS dir

--- ReactJS/Scaler/01-Intro-To-React/02-Dummy-React/lib/ReactDOM.js
+++
@@ -1,8 +0,0 @@
-let ReactDOM = {};
-function render(component,react){
-    let optimizedDOM = react(component);
-    console.log("Rendering to DOM.");
-    root.innerHTML = optimizedDOM;
-}
-
-ReactDOM.render = render;
--- ReactJS/Scaler/03-React-Machine-Coding/todo-app/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  // In dev-strict mode useEffect will be called twice
-  <App />,
-)
