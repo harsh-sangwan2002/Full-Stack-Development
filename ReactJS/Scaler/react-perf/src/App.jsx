fcff01a68cc029767fc57d1c120d0c9a0d842c7e
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-28 20:32:21 +0530

    implemented lazy-loading

--- ReactJS/Scaler/react-perf/src/App.jsx
+++ ReactJS/Scaler/react-perf/src/App.jsx
@@ -1,36 +1,42 @@
 import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
 
 function App() {
 
-  const [ContactPage, setContactPage] = useState(null);
-  const [AboutPage, setAboutPage] = useState(null);
+  // const [ContactPage, setContactPage] = useState(null);
+  // const [AboutPage, setAboutPage] = useState(null);
 
-  const loadContact = () => {
-    import('./components/Contact').then((module) => {
-      setContactPage(() => module.default)
-    })
-  }
+  // const loadContact = () => {
+  //   import('./components/Contact').then((module) => {
+  //     setContactPage(() => module.default)
+  //   })
+  // }
 
-  const loadAbout = () => {
-    import('./components/About').then((module) => {
-      setAboutPage(() => module.default)
-    })
-  }
+  // const loadAbout = () => {
+  //   import('./components/About').then((module) => {
+  //     setAboutPage(() => module.default)
+  //   })
+  // }
+
+  const Home = lazy(() => import('./components/Home'));
+  const Contact = lazy(() => import('./components/Contact'));
+  const About = lazy(() => import('./components/About'));
 
   return (
     <Router>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about" onClick={loadAbout}>About Us</Link></li>
-        <li><Link to="/contact" onClick={loadContact}>Contact</Link></li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={ContactPage ? <ContactPage /> : <div>...loading</div>} />
-        <Route path="/about" element={AboutPage ? <AboutPage /> : <div>...loading</div>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
