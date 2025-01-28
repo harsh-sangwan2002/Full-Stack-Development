6081233eb34fb79ab75d147d9e7a7381f89a1695
Harsh Sangwan <hsangwan2002@gmail.com>
2025-01-28 14:36:06 +0530

    updated merge-overlapping intervals

--- Interview-Prep-DSA/Advanced/DSA-1/Arrays/Interview-Problems/MergeIntervals.java
+++ Interview-Prep-DSA/Advanced/DSA-1/Arrays/Interview-Problems/MergeIntervals.java
@@ -1,23 +1,7 @@
 import java.util.ArrayList;
 
-// Definition for an interval.
-class Interval {
-    int start;
-    int end;
-
-    Interval() {
-        start = 0;
-        end = 0;
-    }
-
-    Interval(int s, int e) {
-        start = s;
-        end = e;
-    }
-}
-
 public class MergeIntervals {
-    
+
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
 
         ArrayList<Interval> res = new ArrayList<>();
