23ca13333845cf2387ee29ceaec4c6bf754435a1
Harsh Sangwan <hsangwan2002@gmail.com>
2025-02-03 19:55:52 +0530

    Minor Changes in concurrency

--- Backend-Development/LLD_1/src/Concurrency01/HelloWorldPrinter.java
+++ Backend-Development/LLD_1/src/Concurrency01/HelloWorldPrinter.java
@@ -4,6 +4,7 @@ public class HelloWorldPrinter implements Runnable{
 
     @Override
     public void run(){
+        System.out.println("Inside HelloWorldPrinter class.");
         System.out.println("Hello World: Thread name = "+Thread.currentThread().getName());
     }
 }
