c9bb14e3c4db6cb86aa0faed3291a00a55f18ffb
Harsh Sangwan <hsangwan2002@gmail.com>
2025-02-12 14:55:35 +0530

    map, filter, reduce done

---
+++ ReactJS/YouTube/Pre-Requisites/Map-Filter-Reduce/map.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4];
+let nArr = arr.map((item) => item * 2);
+
+console.log(arr);
+console.log(nArr);
---
+++ ReactJS/YouTube/Pre-Requisites/Map-Filter-Reduce/reduce.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5];
+let sum = arr.reduce((acc, item) => acc + item, 0);
+
+console.log(sum);
