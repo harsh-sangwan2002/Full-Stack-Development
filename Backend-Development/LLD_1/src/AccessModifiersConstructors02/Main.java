3e8188ea8ca03604c2ea93624907d53645305785
Harsh Sangwan <hsangwan2002@gmail.com>
2025-02-19 21:10:57 +0530

    updated .DS_Store

--- Backend-Development/LLD_1/src/AccessModifiersConstructors02/Main.java
+++ Backend-Development/LLD_1/src/AccessModifiersConstructors02/Main.java
@@ -7,7 +7,9 @@ public class Main  {
         Student stu1 = new Student("Harsh",21,"Sept24");
         Student stu2 = new Student("Harsh");
         Student stu3 = new Student(stu1);
-
-        System.out.println("DEBUG");
+        Student stu4 = stu1;
+        
+        System.out.println(stu1);
+        System.out.println(stu4);
     }
 }
