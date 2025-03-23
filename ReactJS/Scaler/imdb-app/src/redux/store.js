0776eb3470d737e8ae9947520208f52ca95ad5af
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-03-23 21:38:48 +0530

    updated store

--- ReactJS/Scaler/imdb-app/src/redux/store.js
+++ ReactJS/Scaler/imdb-app/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./Counter"; 
+import counterReducer from "./Counter";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
     }
 });
