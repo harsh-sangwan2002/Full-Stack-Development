13a871b5018a8fbe848095c5128c5214a219ed96
Harsh Sangwan <hsangwan2002@gmail.com>
2025-05-07 19:05:32 +0530

    implemented products api

--- NodeJS/Scaler/03-Intro-To-MongoDB-CRUD/server.js
+++ NodeJS/Scaler/03-Intro-To-MongoDB-CRUD/server.js
@@ -4,18 +4,9 @@ const app = express();
 app.use(express.json());
 
 const connectDB = require('./config/db');
+const productRouter = require('./routes/product.route');
 
-const { getAllProducts, getPrductById, createProduct, updateProductById, deleteProductById } = require('./controllers/product.controller');
-
-app.get('/api/products', getAllProducts);
-
-app.get('/api/products/:id', getPrductById);
-
-app.post('/api/products', createProduct);
-
-app.put('/api/products/:id', updateProductById);
-
-app.delete('/api/products/:id', deleteProductById);
+app.use('/api/products', productRouter);
 
 app.listen(3000, async () => {
     console.log("Backend server is running on port 3000");
