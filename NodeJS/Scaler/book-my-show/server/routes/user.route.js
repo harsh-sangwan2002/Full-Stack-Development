7d8edd4a71e16e13cd0e00bf892645fd759a3425
Harsh Sangwan <hsangwan2002@gmail.com>
2025-05-07 18:31:06 +0530

    remove book my show project

--- NodeJS/Scaler/book-my-show/server/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const userRouter = require('express').Router();
-const { loginUser, registerUser } = require('../controllers/user.controller');
-const {authMiddleware} = require('../middlewares/auth.middleware');
-
-userRouter.post('/register', authMiddleware, registerUser);
-userRouter.post('/login', loginUser);
-
-module.exports = userRouter;
