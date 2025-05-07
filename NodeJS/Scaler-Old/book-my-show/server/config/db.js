b531f43b3f6ad9c7d4edb87478df5d814de0ab6f
Harsh Sangwan <hsangwan2002@gmail.com>
2025-05-07 17:22:46 +0530

    remove book-my-show project

--- NodeJS/Scaler-Old/book-my-show/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectToDB = async () => {
-
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
-}
-
-module.exports = connectToDB;
--- NodeJS/Scaler-Old/book-my-show/server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require('express').Router();
-const { registerUser, loginUser, getCurrentUser } = require('../controllers/user.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-userRouter.post('/register', registerUser);
-userRouter.post('/login', loginUser)
-userRouter.get('/', getCurrentUser)
-
-module.exports = userRouter;
