d084bf6b8c7471ac6fb73177ddf5b99332e27754
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-15 08:19:55 +0530

    removed book my show project

--- NodeJS/Scaler/book-my-show/client/src/calls/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-})
