567eea1e298794f03c896b82ba4f6f8f20651968
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-15 10:19:24 +0530

    deleted book my show

--- NodeJS/Scaler/book-my-show/server/routes/movie.route.js
+++
@@ -1,9 +0,0 @@
-const movieRouter = require('express').Router();
-const { AddMovie, GetAllMovies, DeleteMovie, UpdateMovie } = require('../controllers/movie.controller');
-
-movieRouter.get('/', GetAllMovies)
-    .post('/add-movie', AddMovie)
-    .put('/', UpdateMovie)
-    .put('/delete', DeleteMovie)
-
-module.exports = movieRouter;
