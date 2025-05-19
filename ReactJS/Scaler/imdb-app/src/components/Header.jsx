b660a2f968e29c260a7294b221488513f26e0d9f
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-19 11:27:36 +0530

    initialized imdb app

--- ReactJS/Scaler/imdb-app/src/components/Header.jsx
+++ ReactJS/Scaler/imdb-app/src/components/Header.jsx
@@ -1,23 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
 const Header = () => {
     return (
         <div className="header">
-            <h1>
-                <Link to="/">IMDB</Link>
-            </h1>
-            <ul>
-                <li>
-                    <Link to="/"> Movies</Link>
-                </li>
-                <li>
-                    <Link to="/details">Detail</Link>
-                </li>
-                <li>
-                    <Link to="/watchlist">Watch List</Link>
-                </li>
-            </ul>
+            <h1>IMDB</h1>
         </div>
     )
 }
