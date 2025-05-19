516fe5061037be9e7075012743b3318322985f2b
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-19 20:55:18 +0530

    implemented pre/post hooks and mvc

--- NodeJS/Scaler/04-MVC-Pre-Post-Hooks/config/db.js
+++ NodeJS/Scaler/04-MVC-Pre-Post-Hooks/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://hsangwan2002:Samay%40229@cluster0.49hcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url = process.env.MONGO_URL
 
 const connectDB = async () => {
 
