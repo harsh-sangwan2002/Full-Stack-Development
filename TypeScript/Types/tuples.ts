d5309509b7d5802f8b289ed88a7e3f1488f4fc57
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-22 20:04:53 +0530

    remove TS

--- TypeScript/Types/tuples.ts
+++
@@ -1,7 +0,0 @@
-// const stuff :(string|number)[] = [1, 'a', 2, 'b', 3, 'c'];
-
-const color:[string,string,string] = ["red", "green", "blue"];
-type HTTPResponse = [number,string];
-
-const goodRes:HTTPResponse = [200, "OK"];
-const badRes:HTTPResponse = [404, "Not Found"];
