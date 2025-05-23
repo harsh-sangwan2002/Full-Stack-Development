1847877dc044a05e6ff6cf958a3116aa799e56a8
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-23 08:15:22 +0530

    remove imdb-app

--- ReactJS/Scaler/imdb-app/src/pages/MovieDetailPage.jsx
+++
@@ -1,9 +0,0 @@
-const MovieDetailPage = () => {
-    return (
-        <div>
-            <h1 className="text-center mt-3">Movie Detail Page</h1>
-        </div>
-    )
-}
-
-export default MovieDetailPage
