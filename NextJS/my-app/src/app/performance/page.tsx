85f7a61627970e0b0027df2bf4414b544ce2cce1
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-26 17:17:51 +0530

    created my first next.js project

---
+++ NextJS/my-app/src/app/performance/page.tsx
@@ -0,0 +1,9 @@
+function page() {
+  return (
+    <div>
+      Performance Page
+    </div>
+  )
+}
+
+export default page
---
+++ NextJS/my-app/src/app/scale/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Scaler Page
+    </div>
+  )
+}
+
+export default page
