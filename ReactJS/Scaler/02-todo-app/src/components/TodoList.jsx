f419e324e30e5a547113ae26b58128cc6ec564dd
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-05-29 16:34:23 +0530

    updated TodoList

--- ReactJS/Scaler/02-todo-app/src/components/TodoList.jsx
+++ ReactJS/Scaler/02-todo-app/src/components/TodoList.jsx
@@ -3,23 +3,23 @@ import { useState } from "react";
 const TodoList = ({ title }) => {
 
     const [items, setItems] = useState(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"]);
-    let newItem = '';
+    const [newItem, setNewItem] = useState("");
 
     const handleAddTodo = () => {
         // React will only re-render if there is a change in props or state.
         setItems([...items, newItem]);
+        setNewItem('');
     }
 
     const handleChange = (e) => {
-        newItem = e.target.value;
-        console.log(newItem);
+        setNewItem(e.target.value);
     }
 
     return (
         <div>
             <h1>{title || "Todo List"}</h1>
             <div style={{ display: 'flex' }}>
-                <input type="text" placeholder="Enter a todo" onChange={handleChange} />
+                <input type="text" placeholder="Enter a todo" value={newItem} onChange={handleChange} />
                 <button onClick={handleAddTodo}>Add Todo</button>
             </div>
             <ul>
