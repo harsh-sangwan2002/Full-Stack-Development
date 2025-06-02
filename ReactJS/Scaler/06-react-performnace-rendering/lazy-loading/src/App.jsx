b35cdad8cf89b8df096ca50029cf9b42bcdfe18a
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-06-02 14:48:21 +0530

    App.jsx done

--- ReactJS/Scaler/06-react-performnace-rendering/lazy-loading/src/App.jsx
+++ ReactJS/Scaler/06-react-performnace-rendering/lazy-loading/src/App.jsx
@@ -2,23 +2,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import Navbar from './components/Navbar'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useEffect, useState } from 'react'
 import ItemList from './components/ItemList'
 
-const Home = lazy(() => import('./pages/Home/Home'))
 const About = lazy(() => import('./pages/About/About'))
 const Contact = lazy(() => import('./pages/Contact/Contact'))
 const LargeSumArray = lazy(() => import('./components/LargeSumArray'))
 
 function App() {
 
+  const [HomePage, setHomePage] = useState(null);
+  // dynamic import
+  useEffect(() => {
+    import('./components/LargeSumArray')
+      .then(module => {
+        setHomePage(module.export);
+      })
+      .catch(err => {
+        console.error('Error loading LargeSumArray component:', err);
+      });
+  }, [])
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<LargeSumArray />} /> */}
-          <Route path="/" element={<ItemList />} />
+          <Route path="/" element={[HomePage && <HomePage />, <LargeSumArray />, <ItemList />]} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
