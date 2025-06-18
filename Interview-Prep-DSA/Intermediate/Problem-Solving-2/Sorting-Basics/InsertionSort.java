cda045fe31c268329d87b90c9d7e075b58bcfcf4
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-06-18 20:10:52 +0530

    updated insertion sort

--- Interview-Prep-DSA/Intermediate/Problem-Solving-2/Sorting-Basics/InsertionSort.java
+++ Interview-Prep-DSA/Intermediate/Problem-Solving-2/Sorting-Basics/InsertionSort.java
@@ -6,7 +6,7 @@ public class InsertionSort {
 
         int n = arr.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             int j = i - 1;
 
             while (j >= 0 && arr[j] > arr[j + 1]) {
