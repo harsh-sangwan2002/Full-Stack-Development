927116ef8acc81d578b081dbc60523dcd1f306fd
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-06-18 22:04:20 +0530

    removed advanced dsa module

--- Interview-Prep-DSA/Advanced/DSA-1/Bit-Manipulation/Bit-One/ToggleBit.java
+++
@@ -1,8 +0,0 @@
-public class ToggleBit {
-    
-    public int solve(int A, int B) {
-
-        A = A ^ (1 << B);
-        return A;
-    }
-}
--- Interview-Prep-DSA/Advanced/DSA-1/Maths/Modular-Arithmetic-GCD/ABModulo.java
+++
@@ -1,7 +0,0 @@
-public class ABModulo {
-    
-    public int solve(int A, int B) {
-
-        return Math.abs(A - B);
-    }
-}
