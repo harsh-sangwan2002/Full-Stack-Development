d384743eb9ed6bbe75295bec0f30650bf04fd06b
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-07-03 11:12:15 +0530

    prefix sum completed

---
+++ 90-Days-Prep/DSA/Intermediate/Intro-To-Problem-Solving-1/Prefix-Sum/InPlacePrefixSum.java
@@ -0,0 +1,12 @@
+public class InPlacePrefixSum {
+
+    public int[] solve(int[] A) {
+
+        int n = A.length;
+
+        for (int i = 1; i < n; i++)
+            A[i] += A[i - 1];
+
+        return A;
+    }
+}
