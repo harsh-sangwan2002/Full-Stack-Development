8877300f1da37f0893428cc9470f69eceb4d6b60
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-07-09 22:19:10 +0530

    free-code-camp done

---
+++ JavaScript/AE/Free-Code-Camp/comments.js
@@ -0,0 +1,5 @@
+var number = 5; // in-line comment
+
+/*
+    This is a multi-line comment
+*/
---
+++ JavaScript/AE/Free-Code-Camp/variables.js
@@ -0,0 +1,56 @@
+// Data Types : number, string, boolean, null, undefined, bigint, symbol
+var myName = "John Doe"; // string
+var myAge = 30; // number
+var isStudent = false; // boolean
+var myHeight = null; // null
+var myWeight; // undefined
+var myBigInt = 12345678901234567890n; // bigint
+var mySymbol = Symbol("unique"); // symbol
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Variable operations
+var sum = 10 + 10;
+console.log(sum);
+var difference = 10 - 5;
+console.log(difference);
+var product = 10 * 4;
+console.log(product);
+var quotient = 10 / 2;
+console.log(quotient);
+var remainder = 10 % 3;
+console.log(remainder);
+
+// Increment and Decrement
+var myVar = 87;
+myVar++;
+console.log(myVar); // 88
+myVar--;
+console.log(myVar); // 87
+
+// Decimal Numbers
+var ourDecimal = 5.7;
+var myDecimal = 2.3;
+// Multiplying Decimal Numbers
+var productDecimal = ourDecimal * myDecimal;
+console.log(productDecimal);
+// Dividing Decimal Numbers
+var quotientDecimal = ourDecimal / myDecimal;
+console.log(quotientDecimal);
+// Subtracting Decimal Numbers
+var differenceDecimal = ourDecimal - myDecimal;
+console.log(differenceDecimal);
+// Adding Decimal Numbers
+var sumDecimal = ourDecimal + myDecimal;
+console.log(sumDecimal);
+// Finding a Remainder
+var remainderDecimal = ourDecimal % myDecimal;
+console.log(remainderDecimal); 
