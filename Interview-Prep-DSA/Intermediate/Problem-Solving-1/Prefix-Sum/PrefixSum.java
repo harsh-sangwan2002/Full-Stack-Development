1a78b78ee0d5ef66cc8cffec1383e8f2517f2017
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-08-05 20:44:55 +0530

    prefix sum done

---
+++ Interview-Prep-DSA/Intermediate/Problem-Solving-1/Prefix-Sum/PrefixSum.java
@@ -0,0 +1,14 @@
+// TC -> O(N)
+// SC -> O(1)
+public class PrefixSum {
+
+    public int[] solve(int[] A) {
+
+        int n = A.length;
+
+        for (int i = 1; i < n; i++)
+            A[i] += A[i - 1];
+
+        return A;
+    }
+}
