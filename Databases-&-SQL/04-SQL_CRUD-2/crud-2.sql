a5b414708ba3c0ed4d37c03beae6d20aea26ff0b
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-01-16 19:56:34 +0530

    updated crud-2

--- Databases-&-SQL/04-SQL_CRUD-2/crud-2.sql
+++ Databases-&-SQL/04-SQL_CRUD-2/crud-2.sql
@@ -1,61 +1,57 @@
-Select * from sakila.film;
+use sakila;
 
-/* 1. Movies released between 2007 and 2009 [2007,2009] */
-Select * From sakila.film Where release_year>=2006 And release_year<=2009;
+select * from film;
 
-Select * from film
-Where release_year Between 2006 And 2009;
+-- select movies between year 2000 and 2009
+select * from film 
+where release_year >= 2000 and release_year <=2010;
 
-/* 1. Film having "loves" as a substring Using like does pattern match. */
-Select * from film
-Where title Like "%love%";
-
-/* Null means nothing and you can't compare nothing to anything */
-Insert Into film (title,description,release_year,language_id,original_language_id,rental_rate,length,replacement_cost,rating,special_features)
-Values
-('The Dark Knight',null,2008,1,3,4.99,152,19.99,'PG-13','Trailers'),
-('The Dark Knight Rises',null,2012,1,3,4.99,152,19.99,'PG-13','Trailers'),
-('The Dark Knight Returns',null,2016,1,3,4.99,152,19.99,'PG-13','Trailers');
-
--- This comparison doesn't work
-Select * from film
-Where description = Null;
-
--- Correct way
-Select * from film
-Where description Is Null;
-
--- OrderBy clause -> By default it is ascending
--- desciption works as a tie breaker
--- By default tie-breaker is primary key
-Select * from film
-order By release_year, description desc;
+-- using between
+-- between a and b => [a,b] including a and b
+select * from film
+where release_year between 2000 and 2010;
 
 /*
-If you have distinct in the select statement,then you can only sort by the columns that are present in the select statement.
+I want to fetch all films having love as substring in the title
 */
-Select distinct release_year, title From film
-Order By release_year;
+select * from film
+where title like '%love%';
 
-/*
-Umang 2000
-Umang 1999
-Umang 2012
+-- null and Is null
+Insert Into film (title, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features)
+Values('The Dark Knight', 2010, 1, 6, 0.19, 60, 30.01, 'PG', 'Commentaries'),
+('The Dark Knight', 2010, 1, 6, 0.19, 60, 30.01, 'PG', 'Commentaries'),
+('The Dark Knight', 2010, 1, 6, 0.19, 60, 30.01, 'PG', 'Commentaries');
+
+select * from film
+where description Is null;
+
+/* 
+order by 
+asc - ascending (by default)
+desc - descending
 */
+select * from film
+order by title desc;
 
-/* Limit is used to limit the no of rows in the output */
-Select * from film 
-limit 10 offset 100;
+select * from film
+order by release_year desc, description desc;
 
--- Update is to update the data already present in the table
+-- distinct with order by
+select distinct title, release_year from film
+order by release_year;
+
+-- limit is used to limit the number of rows in the output
+select * from film
+limit 10 offset 10;
+
+-- update
 Update film
-Set title = "Skylags", release_year = 2000
-Where film_id = 1;
+set title = 'SkyLags', description = 'Description'
+where film_id = 1;
 
-Select * from film;
+SET SQL_SAFE_UPDATES = 0;
 
--- Delete is used to delete the data from the table
--- Truncate will also delete all the rows and there is no where clause in truncate
--- Drop will also delete the schema of the table
--- Truncate is faster than the delete operator
-Delete from payment Where customer_id = 1;
+-- delete is used to delete rows from the table
+select * from payment;
+delete from payment; -- Going to delete all the rows from the table
